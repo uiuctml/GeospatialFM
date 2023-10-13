b502ffb2587c4e73f089f450b95ab6d35937babe
Ehzoahis <sihaozhe@live.cn>
2023-10-12 21:55:45 -0500

    clf task done

---
+++ launch_train_so2sat.sh
@@ -0,0 +1,12 @@
+# CUDA_VISIBLE_DEVICES=2 python train.py --exp_name dino_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_vit-s16_dino.yaml
+# CUDA_VISIBLE_DEVICES=2 python train.py --exp_name moco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_vit-s16_moco.yaml &
+# CUDA_VISIBLE_DEVICES=3 python train.py --exp_name seco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_seco.yaml
+# CUDA_VISIBLE_DEVICES=2 python train.py --exp_name moco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_moco.yaml &
+# CUDA_VISIBLE_DEVICES=3 python train.py --exp_name dino_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_dino.yaml
+
+CUDA_VISIBLE_DEVICES=2 python train.py --exp_name dino_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_vit-s16_dino.yaml 
+CUDA_VISIBLE_DEVICES=2 python train.py --exp_name moco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_vit-s16_moco.yaml 
+CUDA_VISIBLE_DEVICES=3 python train.py --exp_name seco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_seco.yaml 
+CUDA_VISIBLE_DEVICES=2 python train.py --exp_name moco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_moco.yaml 
+CUDA_VISIBLE_DEVICES=3 python train.py --exp_name dino_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_dino.yaml
+
