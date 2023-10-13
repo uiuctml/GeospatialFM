ba3f6e541cecd62e0634bbe59a3ed92ec5e577e8
Ehzoahis <sihaozhe@live.cn>
2023-10-12 22:00:09 -0500

    clf task done

--- launch_train_bn.sh
+++ launch_train_bn.sh
@@ -1,12 +1,5 @@
-# CUDA_VISIBLE_DEVICES=2 python train.py --exp_name dino_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_vit-s16_dino.yaml
-# CUDA_VISIBLE_DEVICES=2 python train.py --exp_name moco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_vit-s16_moco.yaml &
-# CUDA_VISIBLE_DEVICES=3 python train.py --exp_name seco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_seco.yaml
-# CUDA_VISIBLE_DEVICES=2 python train.py --exp_name moco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_moco.yaml &
-# CUDA_VISIBLE_DEVICES=3 python train.py --exp_name dino_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_dino.yaml
-
-CUDA_VISIBLE_DEVICES=2 python train.py --exp_name dino_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_vit-s16_dino.yaml 
-CUDA_VISIBLE_DEVICES=2 python train.py --exp_name moco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_vit-s16_moco.yaml 
-CUDA_VISIBLE_DEVICES=3 python train.py --exp_name seco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_seco.yaml 
-CUDA_VISIBLE_DEVICES=2 python train.py --exp_name moco_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_moco.yaml 
-CUDA_VISIBLE_DEVICES=3 python train.py --exp_name dino_frac0.1 --config_file GeospatialFM/configs/so2sat/so2sat_rn50_dino.yaml
-
+CUDA_VISIBLE_DEVICES=0 python train.py --exp_name dino_frac0.1 --config_file GeospatialFM/configs/bigearthnet/bn_vit-s16_dino.yaml TRAINER.learning_rate=0.1 MODEL.freeze_encoder=true &
+CUDA_VISIBLE_DEVICES=1 python train.py --exp_name moco_frac0.1 --config_file GeospatialFM/configs/bigearthnet/bn_vit-s16_moco.yaml TRAINER.learning_rate=0.1 MODEL.freeze_encoder=true &
+CUDA_VISIBLE_DEVICES=2 python train.py --exp_name seco_frac0.1 --config_file GeospatialFM/configs/bigearthnet/bn_rn50_seco.yaml TRAINER.learning_rate=0.1 MODEL.freeze_encoder=true &
+CUDA_VISIBLE_DEVICES=3 python train.py --exp_name moco_frac0.1 --config_file GeospatialFM/configs/bigearthnet/bn_rn50_moco.yaml TRAINER.learning_rate=0.1 MODEL.freeze_encoder=true &
+CUDA_VISIBLE_DEVICES=4 python train.py --exp_name dino_frac0.1 --config_file GeospatialFM/configs/bigearthnet/bn_rn50_dino.yaml TRAINER.learning_rate=0.1 MODEL.freeze_encoder=true
