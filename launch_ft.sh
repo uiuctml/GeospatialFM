6c7ea59ba7fd7ad0010a4059af279f1b6a95e82f
Ehzoahis <sihaozhe@live.cn>
2024-02-05 00:16:54 -0600

    sync

--- launch_ft.sh
+++ launch_ft.sh
@@ -1,15 +1,15 @@
-for i in 3e-5 7e-5
+for i in 5e-4
 do
     for j in 5e-3
     do  
         echo "ViT learning_rate: $i, weight_decay: $j"
-        CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node 2 --master_port=10086 -m finetune --exp_name mae_base \
+        CUDA_VISIBLE_DEVICES=1,3 torchrun --nproc_per_node 2 --master_port=10086 -m finetune --exp_name mae_base \
         --config_file GeospatialFM/configs/finetune_vit.yaml --debug \
         MODEL.load_pretrained_from=dir \
         TRAINER.learning_rate=$i TRAINER.weight_decay=$j
 
         echo "CViT learning_rate: $i, weight_decay: $j"
-        CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node 2 --master_port=10086 -m finetune --exp_name mae_vit_btnk \
+        CUDA_VISIBLE_DEVICES=1,3 torchrun --nproc_per_node 2 --master_port=10086 -m finetune --exp_name mae_vit_btnk \
         --config_file GeospatialFM/configs/finetune_cvit.yaml --debug \
         MODEL.load_pretrained_from=dir \
         TRAINER.learning_rate=$i TRAINER.weight_decay=$j
