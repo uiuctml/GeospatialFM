2d0dadd1084ee10165d7f437c5baba6a7c875150
Ehzoahis <sihaozhe@live.cn>
2024-02-14 13:57:08 -0600

    flexible channel vit (before debug)

--- GeospatialFM/models/channel_attn_vit_archived.py
+++ GeospatialFM/models/channel_attn_vit_archived.py
@@ -10,6 +10,46 @@ from typing import Optional
 SENTINEL_WV = [442.7, 492.4, 559.8, 664.6, 704.1, 740.5, 782.8, 832.8, 864.7, 945.1, 1373.5, 1613.7, 2202.4]
 # SENTINEL_WV = [4.427, 4.924, 5.598, 6.646, 7.041, 7.405, 7.828, 8.328, 8.647, 9.451, 13.735, 16.137, 22.024]
 
+def collpase_channels(x, reduce=None):
+    assert len(x.shape) == 3
+    if reduce == "mean":
+        return x.mean(dim=1)
+    elif reduce == "sum":
+        return x.sum(dim=1)
+    elif reduce == "flatten":
+        return x.view(x.shape[0], -1, x.shape[-1])
+    elif reduce is None:
+        return x
+    else:
+        raise NotImplementedError
+
+class ChannelAttention(nn.Module):
+    def __init__(self, embed_dim):
+        super(ChannelAttention, self).__init__()
+        self.query = nn.Linear(embed_dim, embed_dim)
+        self.key = nn.Linear(embed_dim, embed_dim)
+        self.value = nn.Linear(embed_dim, embed_dim)
+        
+    def forward(self, x):
+        # Assuming x is of shape [B, C, E]
+        # Compute query, key, value matrices
+        query = self.query(x)  # Shape [B, C, E]
+        key = self.key(x)      # Shape [B, C, E]
+        value = self.value(x)  # Shape [B, C, E]
+        
+        # Calculate attention scores
+        attention_scores = torch.matmul(query, key.transpose(-2, -1))  # Shape [B, C, C]
+        attention_scores = F.softmax(attention_scores, dim=-1)
+        
+        # Apply attention scores to value matrix
+        weighted_values = torch.matmul(attention_scores, value)  # Shape [B, C, E]
+        
+        # Combine weighted values for all channels
+        combined = weighted_values.sum(dim=1)  # Shape [B, E]
+        
+        return combined
+
+
 class PatchEmbedPerChannel(nn.Module):
     """Image to Patch Embedding."""
 
@@ -20,6 +60,7 @@ class PatchEmbedPerChannel(nn.Module):
         in_chans: int = 3,
         embed_dim: int = 768,
         enable_sample: bool = True,
+        channel_pool: Optional[str] = None,
     ):
         super().__init__()
         num_patches = (img_size // patch_size) * (img_size // patch_size) * in_chans
@@ -91,8 +132,10 @@ class PatchEmbedPerChannel(nn.Module):
             # Update the embedding lookup
             cur_channel_embed = torch.gather(cur_channel_embed, 2, ids_keep.unsqueeze(1).repeat(1, cur_channel_embed.shape[1], 1))
             ######
+            cin = len_keep
         else:
             mask = torch.zeros([1, Cin], device=x.device)
+            cin = Cin
             ids_restore = torch.arange(Cin, device=x.device) # TODO: check this
 
         # shared projection layer across channels
@@ -100,12 +143,12 @@ class PatchEmbedPerChannel(nn.Module):
 
         # channel specific offsets
         x += cur_channel_embed.unsqueeze(-1).unsqueeze(-1)
-        x = x.flatten(3).permute(0, 2, 3, 1)  # B Cout Cin HW -> B Cin HW Cout
+        # x += self.channel_embed[:, :, cur_channels, :, :]  # B Cout Cin H W
 
         # expand mask and ids_restore to B
         mask = mask.expand(B, -1)
         ids_restore = ids_restore.expand(B, -1)
-        return x, mask, ids_restore
+        return x, mask, ids_restore, cin
 
 
 class ChannelViTEncoder(ViTEncoder):
@@ -118,14 +161,13 @@ class ChannelViTEncoder(ViTEncoder):
                  in_chans=3,
                  embed_dim=768,
                  channel_pool='max',
-                 sptial_spectral_blocks=0,
-                 spectral_blocks=0,
+                 pool_position=0,
                  **kwargs):
         super().__init__(img_size, patch_size, in_chans, embed_dim, **kwargs) # TODO: check this
         # Additional args for Channel_ViT
         self.channel_pool = channel_pool
         # override the patch embedding
-        self.patch_embed = PatchEmbedPerChannel(img_size, patch_size, in_chans, embed_dim)
+        self.patch_embed = PatchEmbedPerChannel(img_size, patch_size, in_chans, embed_dim, channel_pool=self.channel_pool)
         if channel_pool == "mean":
             self.channel_pool = nn.AdaptiveAvgPool1d(1)
         elif channel_pool == "max":
@@ -135,68 +177,41 @@ class ChannelViTEncoder(ViTEncoder):
             self.channel_pool = ChannelAttention(embed_dim)
         else:
             raise NotImplementedError
-        assert spectral_blocks + sptial_spectral_blocks <= self.num_layers, "number of total blocks should be less than the number of layers"
-        self.spectral_blocks = spectral_blocks
-        self.sptial_spectral_blocks = sptial_spectral_blocks
-        self.spatial_blocks = self.num_layers - spectral_blocks - sptial_spectral_blocks
-
-    def _pool_channel(self, x):
-        B, Cin, HW, Cout = x.shape
-        if isinstance(self.channel_pool, ChannelAttention):
-            x = x.permute(0, 2, 1, 3).reshape(-1, Cin, Cout) # BHW Cin Cout
-        else:
-            x = x.permute(0, 3, 2, 1).reshape(B, -1, Cin) # B HWCout Cin
-        x = self.channel_pool(x).reshape(B, HW, Cout) # B HW Cout
-        return x
-
-    def _spectral2spatial(self, x, B):
-        """Convert from spectral order to spatial order"""
-        BHW, Cin, Cout = x.shape
-        x = x.view(B, -1, Cin, Cout).permute(0, 2, 1, 3).reshape(B, -1, Cout) # B CinHW Cout
-        return x
-
-    def _spatial2spectral(self, x):
-        """Convert from spatial order to spectral order"""
-        B, Cin, L, Cout = x.shape
-        x = x.permute(0, 2, 1, 3).reshape(-1, Cin, Cout) # BL Cin Cout
-        return x
-
-    def _pool_channel_with_extra_tokens(self, x, cin):
-        post_tokens = x[:, :self.num_register_tokens+1]
-        patch_tokens = x[:, self.num_register_tokens+1:] # [B, CinL, Cout]
-        B, _, Cout = patch_tokens.shape
-        patch_tokens = patch_tokens.view(B, cin, -1, Cout).permute(0, 3, 1, 2) # [B, Cin, L, Cout] -> [B, Cout, Cin, L]
-        patch_tokens = self.channel_pool(patch_tokens) # [B, HW, Cout]
-        x = torch.cat((post_tokens, patch_tokens), dim=1)
-        return x
-
+        assert pool_position in [0, 1]
+        self.pool_position = pool_position
+        
     # ViT Forward path
     def forward(self, x, channel_ids=SENTINEL_WV, return_dict=False):
-        x, _, _ = self.patch_embed(x, channel_mask_ratio=0, channel_ids=channel_ids)
-        B, Cin, HW, Cout = x.shape
-        if self.spatial_blocks == self.num_layers:
-            assert self.sptial_spectral_blocks == 0, "No spectral blocks are allowed"
-            assert self.spectral_blocks == 0, "No spectral blocks are allowed"
-            # Normal ViT
-            x = self._pool_channel(x)
+        x, _, _, _ = self.patch_embed(x, 0, channel_ids)
+        if self.pool_position == 0:
+            if isinstance(self.channel_pool, ChannelAttention):
+                B, Cout, Cin, H, W = x.shape
+                x = x.permute(0, 3, 4, 2, 1).reshape(-1, Cin, Cout)
+                x = self.channel_pool(x).reshape(B, -1, Cout)
+            else:
+                # preparing the output sequence
+                x = x.flatten(3)  # B Cout Cin HW
+                B, Cout, Cin, HW = x.shape
+                x = x.permute(0, 3, 1, 2).reshape(B, -1, Cin) # B HWCout Cin
+                x = self.channel_pool(x).reshape(B, HW, Cout) # B HW Cout
+            
             pos_embed = self.pos_embed[:, 1:, :]
+            x = x + pos_embed # B HW Cout 
         else:
-            pos_embed = self.interpolate_positional_encoder(Cin)
-        x = x + pos_embed # B Cin HW Cout
+            x = x.flatten(3) # B Cout Cin HW
+            x = x.permute(0, 2, 3, 1) # B Cin HW Cout
 
-        # spectral only blocks
-        if self.spectral_blocks > 0:
-            assert x.dim() == 4, "Input tensor should be B Cin L Cout"
-            x = self._spatial2spectral(x) # B Cin HW Cout -> # BHW Cin Cout
-            for blk in self.blocks[:self.spectral_blocks]:
-                x = blk(x)
-            x = self._spectral2spatial(x, B) # BHW Cin Cout -> B CinHW Cout
+            pos_embed = self.interpolate_positional_encoder(cin) # TODO: check this
+            x = x + pos_embed # B Cin HW Cout
+
+        # # add pos embed w/o cls token
+        # x = x + self.pos_embed[:, 1:, :]
 
         # append cls token
         cls_token = self.cls_token + self.pos_embed[:, :1, :]
         cls_tokens = cls_token.expand(x.shape[0], -1, -1)
-        x = torch.cat((cls_tokens, x), dim=1) 
-        
+        x = torch.cat((cls_tokens, x), dim=1)
+
         if self.register_tokens is not None:
             x = torch.cat(
                 (
@@ -205,22 +220,26 @@ class ChannelViTEncoder(ViTEncoder):
                     x[:, 1:],
                 ),
                 dim=1,
-            ) # B CinHW+T Cout
+            )
 
-        if self.sptial_spectral_blocks > 0:
-            assert x.dim() == 3, "Input tensor should be B L Cout"
-            assert x.shape[1] == L * Cin + 1 + self.num_register_tokens
-            for blk in self.blocks[self.spectral_blocks:self.spectral_blocks+self.sptial_spectral_blocks]:
-                x = blk(x)
-            x = self._pool_channel_with_extra_tokens(x, Cin) # B HW+T Cout
-
-        if self.spatial_blocks > 0:
-            assert x.dim() == 3, "Input tensor should be B L Cout"
-            assert x.shape[1] == L + 1 + self.num_register_tokens
-            for blk in self.blocks[self.spectral_blocks+self.sptial_spectral_blocks:]:
-                x = blk(x)
-
+        # apply Transformer blocks
+        for blk in self.blocks:
+            x = blk(x)
         x = self.norm(x)
+
+        if self.pool_position == 1:
+            post_tokens = x[:, :self.num_register_tokens+1]
+            patch_tokens = x[:, self.num_register_tokens+1:] # [B, CinL, Cout]
+            B, _, Cout = patch_tokens.shape
+            patch_tokens = patch_tokens.view(B, cin, -1, Cout) # [B, Cin, L, Cout]
+            if isinstance(self.channel_pool, ChannelAttention):
+                patch_tokens = patch_tokens.permute(0, 2, 1, 3).reshape(-1, cin, Cout)
+                patch_tokens = self.channel_pool(patch_tokens).reshape(B, -1, Cout)
+            else:
+                # preparing the output sequence
+                patch_tokens = patch_tokens.permute(0, 2, 3, 1).reshape(B, -1, Cin) # B LCout Cin
+                patch_tokens = self.channel_pool(patch_tokens).reshape(B, -1, Cout) # B HW Cout
+            x = torch.cat((post_tokens, patch_tokens), dim=1)
 
         cls_token = x[:, 0]
         register_tokens = x[:, 1:self.num_register_tokens+1] if self.num_register_tokens else None
@@ -245,30 +264,30 @@ class ChannelViTEncoder(ViTEncoder):
         # embed patches
         x, _channel_mask, channel_ids_restore, cin = self.patch_embed(x, channel_mask_ratio, channel_ids)
         channel_mask = _channel_mask.unsqueeze(1).expand(-1, self.patch_size**2, -1).flatten(1)
+        if self.pool_position == 0:
+            if isinstance(self.channel_pool, ChannelAttention):
+                B, Cout, Cin, H, W = x.shape
+                x = x.permute(0, 3, 4, 2, 1).reshape(-1, Cin, Cout)
+                x = self.channel_pool(x).reshape(B, -1, Cout)
+            else:
+                # preparing the output sequence
+                x = x.flatten(3)  # B Cout Cin HW
+                B, Cout, Cin, HW = x.shape
+                x = x.permute(0, 3, 1, 2).reshape(B, -1, Cin) # B HWCout Cin
+                x = self.channel_pool(x).reshape(B, HW, Cout) # B HW Cout
 
-        B, Cin, HW, Cout = x.shape
-        if self.spatial_blocks == self.num_layers:
-            assert self.sptial_spectral_blocks == 0, "No spectral blocks are allowed"
-            assert self.spectral_blocks == 0, "No spectral blocks are allowed"
-            # Normal ViT
-            x = self._pool_channel(x)
             pos_embed = self.pos_embed[:, 1:, :]
+            x = x + pos_embed # B HW Cout 
+            # masking: length -> length * mask_ratio
+            x, mask, ids_restore = self.random_masking(x, mask_ratio)           
         else:
-            pos_embed = self.interpolate_positional_encoder(Cin)
-        x = x + pos_embed # B Cin HW Cout
-    
-        x = x + pos_embed # B Cin HW Cout
-        # masking: length -> length * mask_ratio
-        x, mask, ids_restore = self._random_masking(x, mask_ratio) # B Cin L Cout / B L Cout
-        L = x.shape[-2]
+            x = x.flatten(3) # B Cout Cin HW
+            x = x.permute(0, 2, 3, 1) # B Cin HW Cout
 
-        # spectral only blocks
-        if self.spectral_blocks > 0:
-            assert x.dim() == 4, "Input tensor should be B Cin L Cout"
-            x = self._spatial2spectral(x) # B Cin L Cout -> # BL Cin Cout
-            for blk in self.blocks[:self.spectral_blocks]:
-                x = blk(x)
-            x = self._spectral2spatial(x, B) # BHW Cin Cout -> B CinHW Cout
+            pos_embed = self.interpolate_positional_encoder(cin) # TODO: check this
+            x = x + pos_embed # B Cin HW Cout
+            # masking: length -> length * mask_ratio
+            x, mask, ids_restore = self.random_masking_3D(x, mask_ratio)
 
         # append cls token
         cls_token = self.cls_token + self.pos_embed[:, :1, :]
@@ -286,20 +305,24 @@ class ChannelViTEncoder(ViTEncoder):
                 dim=1,
             )
 
-       if self.sptial_spectral_blocks > 0:
-            assert x.dim() == 3, "Input tensor should be B L Cout"
-            assert x.shape[1] == L * Cin + 1 + self.num_register_tokens
-            for blk in self.blocks[self.spectral_blocks:self.spectral_blocks+self.sptial_spectral_blocks]:
-                x = blk(x)
-            x = self._pool_channel_with_extra_tokens(x, Cin) # B L+T Cout
+        # apply Transformer blocks
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x) 
 
-        if self.spatial_blocks > 0:
-            assert x.dim() == 3, "Input tensor should be B L Cout"
-            assert x.shape[1] == L + 1 + self.num_register_tokens
-            for blk in self.blocks[self.spectral_blocks+self.sptial_spectral_blocks:]:
-                x = blk(x)
-
-        x = self.norm(x)
+        if self.pool_position == 1:
+            post_tokens = x[:, :self.num_register_tokens+1]
+            patch_tokens = x[:, self.num_register_tokens+1:] # [B, CinL, Cout]
+            B, _, Cout = patch_tokens.shape
+            patch_tokens = patch_tokens.view(B, cin, -1, Cout) # [B, Cin, L, Cout]
+            if isinstance(self.channel_pool, ChannelAttention):
+                patch_tokens = patch_tokens.permute(0, 2, 1, 3).reshape(-1, cin, Cout)
+                patch_tokens = self.channel_pool(patch_tokens).reshape(B, -1, Cout)
+            else:
+                # preparing the output sequence
+                patch_tokens = patch_tokens.permute(0, 2, 3, 1).reshape(B, -1, cin) # B LCout Cin
+                patch_tokens = self.channel_pool(patch_tokens).reshape(B, -1, Cout) # B HW Cout
+            x = torch.cat((post_tokens, patch_tokens), dim=1)
             
         if return_dict:
             return dict(latent=x, mask=mask, ids_restore=ids_restore, 
@@ -307,21 +330,13 @@ class ChannelViTEncoder(ViTEncoder):
                         # channel_ids_restore=channel_ids_restore, kept_channels=cin)
         return x, mask, ids_restore, channel_mask#, channel_ids_restore, cin
 
-    def _random_masking(self, x, mask_ratio):
-        if x.dim() == 3:
-            return self.random_masking(x, mask_ratio)
-        elif x.dim() == 4:
-            return self.random_masking_3D(x, mask_ratio)
-        else:
-            raise ValueError("Input tensor should be 3D or 4D")
-
     def random_masking_3D(self, x, mask_ratio):
         """
         Perform per-sample random masking by per-sample shuffling.
         Per-sample shuffling is done by argsort random noise.
         x: [N, C, L, D], sequence
         """
-        N, C, L, D = x.shape  # batch, channels, length, dim
+        N, C, L, D = x.shape  # batch, length, dim
         x = x.permute(0, 2, 3, 1).flatten(2)  # [N, L, D*C]
         len_keep = int(L * (1 - mask_ratio))
         
@@ -340,6 +355,6 @@ class ChannelViTEncoder(ViTEncoder):
         mask[:, :len_keep] = 0
         # unshuffle to get the binary mask
         mask = torch.gather(mask, dim=1, index=ids_restore)
-        x_masked = x_masked.view(N, len_keep, D, C).permute(0, 3, 1, 2)  # [N, C, L, D]
+        x_masked = x_masked.view(N, len_keep, D, C).permute(0, 3, 1, 2).reshape(N, -1, D)  # [N, CL, D]
 
         return x_masked, mask, ids_restore
