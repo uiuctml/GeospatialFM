69a5fddd95a69e6d3715a6df77daf1a435208164
Ehzoahis <sihaozhe@live.cn>
2024-02-19 12:34:08 -0600

    sync

--- launch_train.sh
+++ launch_train.sh
@@ -4,11 +4,11 @@
 #     --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
 #     MODEL.OPTICAL.kwargs.spectral_blocks=0 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=0 
 
-CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10083 -m train --exp_name mae_cvit_0-12-0 \
-    --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
-    MODEL.OPTICAL.kwargs.spectral_blocks=0 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=12
+# CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10083 -m train --exp_name mae_cvit_0-12-0 \
+#     --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
+#     MODEL.OPTICAL.kwargs.spectral_blocks=0 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=12
 
-# CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10082 -m train --exp_name mae_cvit_0-1-11 \
+# CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10087 -m train --exp_name mae_cvit_0-1-11 \
 #     --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
 #     MODEL.OPTICAL.kwargs.spectral_blocks=0 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=1 
 
@@ -16,9 +16,9 @@ CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10083 -m trai
 #     --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
 #     MODEL.OPTICAL.kwargs.spectral_blocks=1 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=0 
 
-# CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10082 -m train --exp_name mae_cvit_1-1-10 \
-#     --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
-#     MODEL.OPTICAL.kwargs.spectral_blocks=1 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=1
+CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10086 -m train --exp_name mae_cvit_1-1-10 \
+    --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
+    MODEL.OPTICAL.kwargs.spectral_blocks=1 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=1
 
 # CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10082 -m train --exp_name mae_cvit_4-4-4 \
 #     --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
