a6d4f27b6124aea0d71e0263cbde2fb317a87da2
Ehzoahis <sihaozhe@live.cn>
2024-02-27 16:52:53 -0600

    mm cvit

--- GeospatialFM/models/multi_modal_mae.py
+++ GeospatialFM/models/multi_modal_mae.py
@@ -2,89 +2,63 @@ import torch.nn as nn
 import numpy as np
 import torch
 from .vision_transformer import ViTEncoder
-from .flexible_channel_vit import ChannelViTEncoder
+from .multi_modal_channel_vit import MultiModalChannelViTEncoder
 
-class CrossModalMAEViT(nn.Module):
+class MultiModalMAEViT(nn.Module):
     def __init__(self, 
-                 optical_encoder, 
-                 radar_encoder,
-                 optical_decoder,
-                 radar_decoder,
+                 multimodal_encoder, 
+                 unimodal_decoder,
                  init_logit_scale=np.log(1 / 0.07),
                  init_logit_bias=None,
-                 use_clip=False,
                  ):
         
         super().__init__()
-        self.optical_encoder = optical_encoder
-        self.radar_encoder = radar_encoder
-        self.optical_decoder = optical_decoder
-        self.radar_decoder = radar_decoder 
+        self.encoder = multimodal_encoder
+        self.decoder = unimodal_decoder
 
-        if use_clip:
-            self.logit_scale = nn.Parameter(torch.ones([]) * init_logit_scale)
-            if init_logit_bias is not None:
-                self.logit_bias = nn.Parameter(torch.ones([]) * init_logit_bias)
-            else:
-                self.logit_bias = None
-        else:
-            self.logit_scale = torch.ones([]) * init_logit_scale
-            self.logit_bias = init_logit_bias
+        self.logit_scale = torch.ones([]) * init_logit_scale
+        self.logit_bias = init_logit_bias
 
-    def set_head(self, optical_head, radar_head):
-        self.optical_head = optical_head
-        self.radar_head = radar_head
+    def set_head(self, head):
+        self.head = head
 
-    def forward_recon(self, optical, radar, mask_ratio=0.75, slice_patch_tokens=None, channel_mask_ratio=0.5):
-        # forward optical
-        if isinstance(self.optical_encoder, ChannelViTEncoder):
-            optical_latent, optical_mask, optical_ids_restore, optical_channel_mask = self.optical_encoder.forward_encoder(optical, mask_ratio, channel_mask_ratio)
-        else:
-            optical_latent, optical_mask, optical_ids_restore = self.optical_encoder.forward_encoder(optical, mask_ratio)
-            optical_channel_mask = None
-        optical_recon = self.optical_decoder.forward_decoder(optical_latent, optical_ids_restore, restore_input_dim=True, slice_patch_tokens=slice_patch_tokens)
-        optical_cls_token = optical_latent[:, 0]
-        # forward radar
-        radar_latent, radar_mask, radar_ids_restore = self.radar_encoder.forward_encoder(radar, mask_ratio)
-        radar_recon = self.radar_decoder.forward_decoder(radar_latent, radar_ids_restore, restore_input_dim=True, slice_patch_tokens=slice_patch_tokens)
-        radar_cls_token = radar_latent[:, 0]
-        return_dict= dict(optical_cls_token=optical_cls_token, radar_cls_token=radar_cls_token,
-                    optical_recon=optical_recon, radar_recon=radar_recon,
-                    optical_mask=optical_mask, radar_mask=radar_mask,)
+    def forward_recon(self, optical=None, radar=None, mask_ratio=0.75, slice_patch_tokens=None, channel_mask_ratio=0.5):
+        latent, mask, ids_restore, optical_channel_mask = self.encoder.forward_encoder(optical=optical, radar=radar, mask_ratio=mask_ratio, channel_mask_ratio=channel_mask_ratio)
+        recon = self.decoder.forward_decoder(latent, ids_restore, restore_input_dim=True, slice_patch_tokens=slice_patch_tokens)
+        cls_token = latent[:, 0]
+
+        return_dict= dict(cls_token=cls_token, recon=recon, mask=mask)
         if optical_channel_mask is not None:
             return_dict['optical_channel_mask'] = optical_channel_mask
         return return_dict
 
-    def forward(self, optical, radar, mask_ratio=0.75, channel_mask_ratio=0.5):
-        # forward optical
-        if isinstance(self.optical_encoder, ChannelViTEncoder):
-            optical_latent, optical_mask, optical_ids_restore, optical_channel_mask = self.optical_encoder.forward_encoder(optical, mask_ratio, channel_mask_ratio)
-        else:
-            optical_latent, optical_mask, optical_ids_restore = self.optical_encoder.forward_encoder(optical, mask_ratio)
-            optical_channel_mask = None
-        optical_recon = self.optical_decoder.forward_decoder(optical_latent, optical_ids_restore)
-        optical_target = self.optical_decoder.forward_target(optical)
-        optical_cls_token = optical_latent[:, 0]
-        # forward radar
-        radar_latent, radar_mask, radar_ids_restore = self.radar_encoder.forward_encoder(radar, mask_ratio)
-        radar_recon = self.radar_decoder.forward_decoder(radar_latent, radar_ids_restore)
-        radar_target = self.radar_decoder.forward_target(radar)
-        radar_cls_token = radar_latent[:, 0]
+    def _forward(self, optical, radar, mask_ratio=0.75, channel_mask_ratio=0.5, prefix=''):
+        latent, mask, ids_restore, optical_channel_mask = self.encoder.forward_encoder(optical=optical, radar=radar, mask_ratio=mask_ratio, channel_mask_ratio=channel_mask_ratio)
+        recon = self.decoder.forward_decoder(latent, ids_restore)
+        cls_token = latent[:, 0]
+
         # return dict
-        return_dict= dict(optical_mask=optical_mask, radar_mask=radar_mask,
-                    optical_recon=optical_recon, radar_recon=radar_recon,
-                    optical_target=optical_target, radar_target=radar_target,
-                    optical_cls_token=optical_cls_token, radar_cls_token=radar_cls_token,
-                    logit_scale=self.logit_scale.exp())
+        return_dict= {f'{prefix}mask': mask, 
+                      f'{prefix}recon': recon, 
+                      f'{prefix}cls_token': cls_token
+                      }
         if optical_channel_mask is not None:
-            return_dict['optical_channel_mask'] = optical_channel_mask
-        if self.logit_bias is not None:
-            return_dict['logit_bias'] = self.logit_bias
-        # downstream head
-        if hasattr(self, 'optical_head'):
-            optical_logits = self.optical_head(optical_cls_token)
-            return_dict['optical_logits'] = optical_logits
-        if hasattr(self, 'radar_head'):
-            radar_logits = self.radar_head(radar_cls_token)
-            return_dict['radar_logits'] = radar_logits
+            return_dict[f'{prefix}channel_mask'] = optical_channel_mask
+        return return_dict
+    
+    def forward(self, optical, radar, mask_ratio=0.75, channel_mask_ratio=0.5, modal=None):
+        assert modal in ['multi', 'optical', 'radar', None]
+        optical_target = self.decoder.forward_target(optical)
+        radar_target = self.decoder.forward_target(radar)
+        return_dict = dict(optical_target=optical_target, radar_target=radar_target, logit_scale=self.logit_scale.exp())
+        # only optical is available
+        if modal is None or modal == 'optical':
+            optical_dict = self._forward(optical, None, mask_ratio, channel_mask_ratio, prefix='optical_')
+            return_dict.update(optical_dict)
+        if modal is None or modal == 'radar':
+            radar_dict = self._forward(None, radar, mask_ratio, channel_mask_ratio, prefix='radar_')
+            return_dict.update(radar_dict)
+        if modal is None or modal == 'multi':
+            multi_dict = self._forward(optical, radar, mask_ratio, channel_mask_ratio, prefix='multi_')
+            return_dict.update(multi_dict)
         return return_dict
--- launch_train.sh
+++ launch_train.sh
@@ -8,17 +8,17 @@
     # --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
     # MODEL.OPTICAL.kwargs.spectral_blocks=2 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=2
 
-CUDA_VISIBLE_DEVICES=6,7 torchrun --nproc_per_node 2 --master_port=10087 -m train --exp_name mae_cvit_2-0-10 \
-    --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
-    MODEL.OPTICAL.kwargs.spectral_blocks=2 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=10
-
-CUDA_VISIBLE_DEVICES=6,7 torchrun --nproc_per_node 2 --master_port=10082 -m train --exp_name mae_cvit_0-2-10 \
-    --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
-    MODEL.OPTICAL.kwargs.spectral_blocks=0 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=2
-
-# CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10086 -m train --exp_name mae_cvit_1-1-10 \
+# CUDA_VISIBLE_DEVICES=6,7 torchrun --nproc_per_node 2 --master_port=10087 -m train --exp_name mae_cvit_2-0-10 \
 #     --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
-#     MODEL.OPTICAL.kwargs.spectral_blocks=1 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=1
+#     MODEL.OPTICAL.kwargs.spectral_blocks=2 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=10
+
+# CUDA_VISIBLE_DEVICES=6,7 torchrun --nproc_per_node 2 --master_port=10082 -m train --exp_name mae_cvit_0-2-10 \
+#     --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
+#     MODEL.OPTICAL.kwargs.spectral_blocks=0 MODEL.OPTICAL.kwargs.sptial_spectral_blocks=2
+
+CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node 2 --master_port=10086 -m train --exp_name mae_cvit_1-2-9 \
+    --config_file GeospatialFM/configs/pretrain_mm_cvit.yaml --debug \
+    MODEL.MULTI_MODAL.kwargs.spectral_blocks=1 MODEL.MULTI_MODAL.kwargs.sptial_spectral_blocks=2
 
 # CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10082 -m train --exp_name mae_cvit_4-4-4 \
 #     --config_file GeospatialFM/configs/pretrain_cvit_v2.yaml \
