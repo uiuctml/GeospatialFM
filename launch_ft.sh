175cf85194f9335e6165c70f10cc9206f1bd0b81
Ehzoahis <sihaozhe@live.cn>
2024-04-16 01:44:34 -0500

    sync

--- launch_ft.sh
+++ launch_ft.sh
@@ -1,4 +1,4 @@
-for i in 2e-4 3e-4
+for i in 7e-4
 do
     for j in 5e-4
     do  
@@ -14,12 +14,42 @@ do
         # MODEL.load_pretrained_from=dir \
         # TRAINER.learning_rate=$i TRAINER.weight_decay=$j
 
+        # echo "CViT learning_rate: $i, weight_decay: $j"
+        # CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10086 -m finetune --exp_name mm_lr_vit_1-0-11 \
+        # --config_file GeospatialFM/configs/finetune_mm_cvit.yaml --debug \
+        # MODEL.load_pretrained_from=dir \
+        # TRAINER.learning_rate=$i TRAINER.weight_decay=$j \
+        # MODEL.MULTI_MODAL.kwargs.spectral_blocks=1 MODEL.MULTI_MODAL.kwargs.sptial_spectral_blocks=0 MODEL.MULTI_MODAL.kwargs.low_rank_feature=true
+
+        # echo "CViT learning_rate: $i, weight_decay: $j"
+        # CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node 2 --master_port=10086 -m finetune --exp_name mm_lr_vit_1-2-9_ms \
+        # --config_file GeospatialFM/configs/finetune_mm_cvit.yaml --debug \
+        # MODEL.load_pretrained_from=dir \
+        # TRAINER.learning_rate=$i TRAINER.weight_decay=$j \
+        # MODEL.MULTI_MODAL.kwargs.spectral_blocks=1 MODEL.MULTI_MODAL.kwargs.sptial_spectral_blocks=2 MODEL.MULTI_MODAL.kwargs.low_rank_feature=true
+
         echo "CViT learning_rate: $i, weight_decay: $j"
-        CUDA_VISIBLE_DEVICES=6,7 torchrun --nproc_per_node 2 --master_port=10086 -m finetune --exp_name mm_lr_vit_1-2-9_fast \
+        CUDA_VISIBLE_DEVICES=4,5 torchrun --nproc_per_node 2 --master_port=10086 -m finetune --exp_name mm_lr_vit_1-2-9 \
         --config_file GeospatialFM/configs/finetune_mm_cvit.yaml --debug \
         MODEL.load_pretrained_from=dir \
         TRAINER.learning_rate=$i TRAINER.weight_decay=$j \
-        MODEL.MULTI_MODAL.kwargs.spectral_blocks=1 MODEL.MULTI_MODAL.kwargs.sptial_spectral_blocks=2 MODEL.MULTI_MODAL.kwargs.low_rank_feature=true
+        MODEL.MULTI_MODAL.kwargs.spectral_blocks=1 MODEL.MULTI_MODAL.kwargs.sptial_spectral_blocks=2 MODEL.MULTI_MODAL.kwargs.low_rank_feature=true MODEL.MULTI_MODAL.kwargs.dim_ratio=0.25
+
+        # echo "CViT learning_rate: $i, weight_decay: $j"
+        # CUDA_VISIBLE_DEVICES=6,7 torchrun --nproc_per_node 2 --master_port=10086 -m finetune --exp_name mm_lr_vit_1-2-9_d4 \
+        # --config_file GeospatialFM/configs/finetune_mm_cvit.yaml --debug \
+        # MODEL.load_pretrained_from=dir \
+        # TRAINER.learning_rate=$i TRAINER.weight_decay=$j \
+        # MODEL.MULTI_MODAL.kwargs.spectral_blocks=1 MODEL.MULTI_MODAL.kwargs.sptial_spectral_blocks=2 MODEL.MULTI_MODAL.kwargs.low_rank_feature=true MODEL.MULTI_MODAL.kwargs.dim_ratio=0.25
+
+        # echo "CViT learning_rate: $i, weight_decay: $j"
+        # CUDA_VISIBLE_DEVICES=6,7 torchrun --nproc_per_node 2 --master_port=10086 -m finetune --exp_name mm_lr_vit_1-2-9_d2_avg \
+        # --config_file GeospatialFM/configs/finetune_mm_cvit.yaml --debug \
+        # MODEL.load_pretrained_from=dir \
+        # TRAINER.learning_rate=$i TRAINER.weight_decay=$j \
+        # MODEL.MULTI_MODAL.kwargs.spectral_blocks=1 MODEL.MULTI_MODAL.kwargs.sptial_spectral_blocks=2 MODEL.MULTI_MODAL.kwargs.low_rank_feature=true MODEL.MULTI_MODAL.kwargs.dim_ratio=0.5
+
+
 
         # echo "CViT learning_rate: $i, weight_decay: $j"
         # CUDA_VISIBLE_DEVICES=6,7 torchrun --nproc_per_node 2 --master_port=10085 -m finetune --exp_name mm_lr_vit_1-2-9_fast \
