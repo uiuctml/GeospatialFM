e973988b3259cbef11699540b99b0088adf5a8b2
Ehzoahis <sihaozhe@live.cn>
2024-10-30 20:36:05 -0500

    8 gpu trianing

--- launch_train.sh
+++ launch_train.sh
@@ -1,10 +1,9 @@
 ROOT_DIR="/home/haozhesi/Dropbox/GeospatialFM"
 export PYTHONPATH=$PYTHONPATH:$ROOT_DIR
 export NCCL_BLOCKING_WAIT=1
-export CUDA_VISIBLE_DEVICES=0,1,2,3
 accelerate launch GeospatialFM/scripts/train.py \
     --data_dir $ROOT_DIR/data/geospatial/SSL4EO \
-    --train_batch_size 16 \
+    --train_batch_size 8 \
     --gradient_accumulation_steps 4 \
     --num_train_epochs 100 \
     --learning_rate 1.5e-4 \
@@ -13,7 +12,7 @@ accelerate launch GeospatialFM/scripts/train.py \
     --channel_mask_ratio 0.5 \
     --lr_warmup_steps 80000 \
     --report_to wandb \
-    --save_steps 500 \
+    --save_steps 1000 \
     --save_total_limit 5 \
     --seed 42 \
     --mixed_precision bf16 \
@@ -22,17 +21,16 @@ accelerate launch GeospatialFM/scripts/train.py \
     --output_dir $ROOT_DIR/results/models \
     --logging_dir $ROOT_DIR/results/logs \
     --wandb_dir $ROOT_DIR/results/ \
-    --run_name LRSSVIT \
+    --run_name LESSVIT_b2 \
     --lr_scheduler cosine \
-    --channel_embed_dims_per_head 1 \
-    --decoder_channel_embed_dims_per_head 1 \
+    --channel_embed_dims_per_head 2 \
+    --decoder_channel_embed_dims_per_head 2 \
     --decoder_depth 8 \
     --decoder_out_chans 15 \
     --use_perception_field_mask \
     --resume_from_checkpoint latest \
-    # --early_stop_steps 2000 \
-    # --modal_mode multi
-    # --max_grad_norm 1.0 \
-    # --norm_pix_loss \
-    # --use_8bit \
-    # --resume_from_checkpoint /home/haozhesi/Dropbox/GeospatialFM/pretrained_models/mae_pretrained_model.pth \
+    --max_grad_norm 1.0 \
+    --proj_drop 0.1 \
+    --attn_drop 0.1 \
+    --loss_type mse \
+    
